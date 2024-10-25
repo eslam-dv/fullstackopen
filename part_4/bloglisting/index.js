d992f15f3fb6b9674dc46e1b7f7c589a0286594f
Eslam Osama <eslam@hp.eslam.local>
2024-10-25 13:33:32 +0300

    part6

--- part_4/bloglisting/index.js
+++
@@ -1,7 +0,0 @@
-import app from "./app.js";
-import { PORT } from "./utils/config.js";
-import { logInfo } from "./utils/logger.js";
-
-app.listen(PORT, () => {
-	logInfo(`Server running on port ${PORT}`);
-});
--- part_4/bloglisting/utils/logger.js
+++
@@ -1,9 +0,0 @@
-const logInfo = (...params) => {
-  console.log(...params);
-}
-
-const logError = (...params) => {
-  console.error(...params);
-}
-
-export { logInfo, logError };
